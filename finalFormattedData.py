71f506463176f82726baa0c8bed074d5a5e77f6d
Harsh <harshbhatia1605@gmail.com>
2024-04-07 00:06:51 +0530

    Fix formatting issues in JSON files and Python scripts

--- finalFormattedData.py
+++ finalFormattedData.py
@@ -1,34 +1,33 @@
 import json
 
 with open("indianArticles.json", "r") as f:
-  articles = json.load(f)
+    articles = json.load(f)
 
 with open("IndianProfs.json", "r") as f:
-  profs = json.load(f)
+    profs = json.load(f)
 
 websiteData = {}
 
 for i in articles:
-  for j in articles[i]:
-    if j["year"] not in websiteData:
-      websiteData[j["year"]] = []
-    try:
-      websiteData[j["year"]].append(
-        {
-          "author":i,
-          "affiliation":profs[i]["affiliation"],
-          "points":j["points"]
-        }
-      )
-    except:
-      print(f"Error with {i} - {j['venue']} - {j['year']}")
-      continue
+    for j in articles[i]:
+        if j["year"] not in websiteData:
+            websiteData[j["year"]] = []
+        try:
+            websiteData[j["year"]].append(
+                {
+                    "author": i,
+                    "affiliation": profs[i]["affiliation"],
+                    "points": j["points"],
+                }
+            )
+        except:
+            print(f"Error with {i} - {j['venue']} - {j['year']}")
+            continue
 
 # Sort the websiteData by key of the dictionary
 websiteData = dict(sorted(websiteData.items()), reverse=True)
 
 with open("websiteData.json", "w") as f:
-  json.dump(websiteData, f, indent=2)
+    json.dump(websiteData, f, indent=2)
 
 print("Done")
-
--- formatIndianProfs.py
+++ formatIndianProfs.py
@@ -1,15 +1,27 @@
 import json
 
 with open("profsName.json", "r") as f:
-  data = json.load(f)
+    data = json.load(f)
 
 data2 = {}
 
-key = ["IIT ", "IIIT ", "National Institute Of Technology", "BITS ", "IISc ", "ISI ", "Tata Inst"]
+key = [
+    "IIT ",
+    "IIIT ",
+    "National Institute Of Technology",
+    "BITS ",
+    "IISc ",
+    "ISI ",
+    "Tata Inst",
+]
 for i in data:
-  for k in key:
-    if (k in data[i]["affiliation"] or data[i]["affiliation"] == "CMI" or data[i]["affiliation"] == "IMSc"):
-      data2[i] = data[i]
+    for k in key:
+        if (
+            k in data[i]["affiliation"]
+            or data[i]["affiliation"] == "CMI"
+            or data[i]["affiliation"] == "IMSc"
+        ):
+            data2[i] = data[i]
 
 with open("IndianProfs.json", "w") as f:
-  json.dump(data2, f, indent=2)
+    json.dump(data2, f, indent=2)
