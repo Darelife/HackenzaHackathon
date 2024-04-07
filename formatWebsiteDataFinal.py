2c07b222d21f83f947a7322272531154474daa99
Prakhar Bhandari <prakharb2k6@gmail.com>
2024-04-07 18:22:34 +0530

    all the data has been uploaded

--- formatWebsiteDataFinal.py
+++ formatWebsiteDataFinal.py
@@ -1,72 +1,5 @@
 import json
 
-with open("indianArticles.json", "r") as f:
-    articles = json.load(f)
-
-with open("IndianProfs.json", "r") as f:
-    profs = json.load(f)
-
-with open("conferenceAcronyms.json", "r") as f:
-    confsAccronyms = json.load(f)
-
-with open("./FieldOfResearch/for.json", "r") as f:
-    forData = json.load(f)
-
-websiteData = {}
-
-for i in articles:
-    for j in articles[i]:
-        if j["year"] not in websiteData:
-            websiteData[j["year"]] = []
-        try:
-            try:
-                # if conference
-
-                if j["venue"] in confsAccronyms:
-                    forr = []
-                    for k in confsAccronyms[j["venue"]]["FOR"]:
-                        try:
-                            forr.append(forData[k])
-                        except:
-                            pass
-                    websiteData[j["year"]].append(
-                        {
-                            "author": i,
-                            "affiliation": profs[i]["affiliation"],
-                            "points": j["points"],
-                            "FOR": forr,
-                        }
-                    )
-                # if journal
-                else:
-                    websiteData[j["year"]].append(
-                        {
-                            "author": i,
-                            "affiliation": profs[i]["affiliation"],
-                            "points": j["points"],
-                            "FOR": "Journal",
-                        }
-                    )
-            except:
-                websiteData[j["year"]].append(
-                    {
-                        "author": i,
-                        "affiliation": profs[i]["affiliation"],
-                        "points": j["points"],
-                        "FOR": "Journal",
-                    }
-                )
-        except:
-            print(f"Error with {i} - {j['venue']} - {j['year']}")
-            continue
-
-# Sort the websiteData by key of the dictionary
-websiteData = dict(sorted(websiteData.items()), reverse=True)
-
-with open("websiteData.json", "w") as f:
-    json.dump(websiteData, f, indent=2)
-
-
 with open("websiteData.json", "r") as f:
     websiteData = json.load(f)
 
@@ -178,6 +111,3 @@ newWebsiteDataAffiliation = dict(
 
 with open("websiteDataAffiliation.json", "w") as f:
     json.dump(newWebsiteDataAffiliation, f, indent=4)
-
-
-print("Done")
